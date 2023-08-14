6c1edc3ce0daf9c0e356f625fdb862e118081831
RNipuni <radikanipuni98@gmail.com>
2023-08-14 14:06:04 +0530

    create booking.php

---
+++ booking.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Home(Hotel management)</title>
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+        <link href="img/royal.png" type="img/icon" rel="icon">
+		
+		
+</head>
+<body>
+    <div id="full">
+        <div style="background-image:url('img/bms.jpg');background-size100% 750px; ;width:100%;height:750px">
+        <div id="header">
+            <div id="logo">
+                <h1><font color="white">Royal Hotel</font></h1>
+</div>
+<div><button id="btn">|||</button></div>
+<div id="nav">
+    <ul><li> <a href="a">Home</a></li>
+    <li> <a href="a">Contact Us</a></li>
+    <li> <a href="a">Booking</a></li>
+    <li> <a href="a">Our hotel</a></li>
+    <li> <a href="a">Help</a></li>
+</ul>
+</div>
+    </div>
+    <div id="banner"></div>
+	<center>
+<div style="background-image:rgba(255,255,255,.5);width:80%;">
+    <table style="color:red;">
+        <tr>
+            <th width="20%" height="50px">Destination</th>
+            <th width="20%" height="50px">Check in date</th>
+            <th width="20%" height="50px">Check out date</th>
+            <th width="20%" height="50px">Room</th>
+			<td rowspan="2"><input type="submit" value="cheack" name="sub"></td>
+</tr>
+<tr>
+            <td width="20%" height="50px"><center><input type="text" name="d1" placeholder="Enter Destination"></center></td>
+            <td width="20%" height="50px"><center><input type="date" name="ci"></center></td>
+            <td width="20%" height="50px"><center><input type="date" name="co"></center></td>
+            <td width="20%" height="50px">
+                <center><select name="room">
+                    <option>1</option>
+                    <option>2</option>
+                    <option>3</option>
+                    <option>4</option>
+                    <option>5</option></center>
+</select>
+            </td>
+</tr>
+    </table>
+</div>
+</center>
+</div>
+
+</div>
+</body>
+    </html>
+
